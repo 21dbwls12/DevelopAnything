ebb34e7153a5e3e535f04ddb041eadd5b04b9c5f
bbangiee <yoojin1117@gmail.com>
2024-02-01 00:00:39 +0900

    change activity, add topAppBaar, add NavigationBar

---
+++ app/src/main/java/com/example/developanything/LeftActivity.kt
@@ -0,0 +1,28 @@
+package com.example.developanything
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Lock
+import com.example.developanything.ui.theme.DevelopAnythingTheme
+
+class LeftActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DevelopAnythingTheme {
+                val currentNumber = 0
+
+                AllScaffold(
+                    title = "2번",
+                    iconButton = { MoveBackIcon() },
+                    content = {
+                        Greeting(Icons.Filled.Lock, currentNumber)
+                    },
+                    currentNumber = currentNumber
+                )
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/developanything/RightActivity.kt
@@ -0,0 +1,28 @@
+package com.example.developanything
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import com.example.developanything.ui.theme.DevelopAnythingTheme
+
+class RightActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DevelopAnythingTheme {
+                val currentNumber = 2
+
+                AllScaffold(
+                    title = "3번",
+                    iconButton = { MoveBackIcon() },
+                    content = {
+                        Greeting(Icons.Filled.Favorite, currentNumber)
+                    },
+                    currentNumber = currentNumber
+                )
+            }
+        }
+    }
+}
