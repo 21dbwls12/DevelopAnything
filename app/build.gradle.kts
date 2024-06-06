5ce59373e842afa7d365465cc6289317f0bdd6be
bbangiee <yoojin1117@gmail.com>
2024-06-06 21:27:45 +0900

    fix: Error in gradle
    - Emulator doesn't work
    - Error message
        - 12 files found with path 'META-INF/INDEX.LIST'.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.android.build.api.dsl.Packaging
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -46,7 +48,7 @@ android {
     }
     packaging {
         resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            excludes += "/META-INF/*"
         }
     }
 }
