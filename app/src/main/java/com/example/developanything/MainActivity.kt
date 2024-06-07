5bcae55ed2fb1860b06a7fa6dc56b4d4652cd0bd
bbangiee <yoojin1117@gmail.com>
2024-06-07 19:17:59 +0900

    feat: Add function for UI
    - When click button content will change
    - for using different server

--- app/src/main/java/com/example/developanything/MainActivity.kt
+++ app/src/main/java/com/example/developanything/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MainScreen("Android")
+                    MainScreen()
                 }
             }
         }
--- app/src/main/java/com/example/developanything/screen/MainScreen.kt
+++ app/src/main/java/com/example/developanything/screen/MainScreen.kt
@@ -1,25 +1,72 @@
 package com.example.developanything.screen
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.developanything.ui.theme.DevelopAnythingTheme
+import androidx.compose.ui.text.rememberTextMeasurer
+import androidx.compose.ui.text.style.TextAlign
 
 @Composable
-fun MainScreen(name: String, modifier: Modifier = Modifier) {
+fun MainScreen() {
+    var clickRetrofit by remember { mutableStateOf(true) }
+    var clickKtor by remember { mutableStateOf(false) }
+
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            ChoiceButton(weight = 0.5f, text = "retrofit2") {
+                clickRetrofit = true
+                clickKtor = false
+            }
+            ChoiceButton(weight = 1f, text = "ktor") {
+                clickRetrofit = false
+                clickKtor = true
+            }
+        }
+        if (clickRetrofit) {
+            RetrofitPage("retrofit2")
+        } else {
+            KtorPage("ktor")
+        }
+    }
+}
+
+@Composable
+fun ChoiceButton(weight: Float, text: String, onClick: () -> Unit) {
+    Button(
+        onClick = onClick,
+        modifier = Modifier.fillMaxWidth(weight)
+    ) {
+        Text(
+            text = text,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
+}
+
+@Composable
+fun RetrofitPage(name: String) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
     )
 }
 
-
-
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    DevelopAnythingTheme {
-        MainScreen("Android")
-    }
+fun KtorPage(name: String) {
+    Text(
+        text = "Hello $name!",
+    )
 }
